76abfa7762b68ce4780b9066def391c20f1dfe32
Gauri Bhagwat <gauribhagwat@gauris-macbook-air.local>
2018-03-09 16:05:39 +0530

    TODO list app done with delegate and dataSource

---
+++ TODOAPP/TodoListViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  TODOAPP
+//
+//  Created by Gauri Bhagwat on 08/03/18.
+//  Copyright © 2018 Development. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+  let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogoron"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+    }
+    
+   //MARK - Tableview Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+
+    //MARK - Tableview Delegate Methods.
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // print(itemArray[indexPath.row])
+        
+        
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+    }
+    
+
+    
+}
+
